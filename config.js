9faf1726af96baabf55e7dfaae921ccecd7a8123
Sam C <sam@xeue.uk>
2022-10-27 00:39:29 +0100

    Tweaks

--- config.js
+++ config.js
@@ -2,7 +2,6 @@ import fs from 'fs'
 import {log, logObj, logs} from 'xeue-logs'
 import reader from 'readline-sync'
 
-
 const defaults = {}
 const required = {}
 const questions = {}
@@ -19,8 +18,8 @@ const config = {
 			}
 			return true
 		} catch (error) {
-			log('There is an error with the config file or it doesn\'t exist', 'E')
-			logObj('Message', error, 'E')
+			log('There is an error with the config file or it doesn\'t exist', 'W')
+			logObj('Message', error, 'W')
 			return false
 		}
 	},
@@ -37,12 +36,18 @@ const config = {
 						log(`${logs.dim}(${required[key].join(', ')})${logs.reset}`, ['H', '', logs.c])
 					}
 				}
-				config[key] = reader.question(`${logs.reset}[ User Input ] ${logs.c}      :${logs.reset} ${logs.g}`)
+				let input = reader.question(`${logs.reset}[ User Input ] ${logs.c}      :${logs.reset} ${logs.c}`)
+				if (input == 'false') input = false
+				if (input == 'true') input = true
+				config[key] = input
 				if (typeof required[key] !== 'undefined') {
 					if (required[key].length > 0 ) {
 						while (!required[key].includes(config[key])) {
 							log(`Invalid value for ${logs.y}${key}${logs.reset} entered, valid values are: ${logs.dim}(${required[key].join(', ')})${logs.reset}`, ['H', '', logs.c])
-							config[key] = reader.question(`${logs.reset}[ User Input ] ${logs.c}      :${logs.reset} ${logs.g}`)
+							let input = reader.question(`${logs.reset}[ User Input ] ${logs.c}      :${logs.reset} ${logs.c}`)
+							if (input == 'false') input = false
+							if (input == 'true') input = true
+							config[key]
 						}
 					}
 				}
@@ -50,7 +55,7 @@ const config = {
 		}
 		if (filePath) {
 			log(`
-Saving configuration to ${filePath}`, ['H', '', logs.c])
+Saving configuration to ${logs.c}${filePath}${logs.reset}`, ['H', '', logs.c])
 			fs.writeFileSync(filePath, JSON.stringify(config.all()))
 		}
 		log(`
@@ -92,7 +97,7 @@ Finished configuration`, ['H', '', logs.c])
 	print: () => {
 		for (const key in config.all()) {
 			if (Object.hasOwnProperty.call(config.all(), key)) {
-				log(`Configuration option ${logs.y}${key}${logs.reset} has been set to: ${logs.b}${config.get(key)}${logs.reset}`, ['H', 'CONFIG', logs.c])
+				log(`Configuration option ${logs.y}${key}${logs.reset} has been set to: ${logs.c}${config.get(key)}${logs.reset}`, ['H', 'CONFIG', logs.c])
 			}
 		}
 	}
